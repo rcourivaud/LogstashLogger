1ee9f543f9a5a12237f4c24381b8867dc9137fc8
Nicolas Vo <nicolas.vo@MBP-C02WC1F4HV2Q.local>
2018-05-22 09:33:00 +0200

    add import

--- logstash_logger/__init__.py
+++ logstash_logger/__init__.py
@@ -2,8 +2,8 @@
 
 """Top-level package for Logstash Logger."""
 
-#__author__ = """Raphaël Courivaud"""
-#__email__ = 'r.courivaud@gmail.com'
-#__version__ = '0.1.0'
+__author__ = """Raphaël Courivaud"""
+__email__ = 'r.courivaud@gmail.com'
+__version__ = '0.1.0'
 
-#from logstash_logger import LogstashLogger
+from .logstash_logger import LogstashLogger
