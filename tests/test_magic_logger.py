a5594c2276cc4e50a4df9f21c53e1ab9980ae402
Nicolas <nicolasvo95@gmail.com>
2018-05-28 23:05:28 +0200

    add unit test

--- tests/test_magic_logger.py
+++ tests/test_magic_logger.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Tests for `logstash_logger` package."""
+"""Tests for `magic_logger` package."""
 
-def test():
-    pass
+from magic_logger import MagicLogger
+import os
+
+TestMagicLogger = MagicLogger(logger_name='test_name', file_name='test_output.txt', host=None)
+
+def test_file_created():
+    assert isinstance(TestMagicLogger, MagicLogger)
+    test_message = 'test_message'
+    TestMagicLogger.info(test_message)
+    assert os.path.exists('test_output.txt') == True
+
+    # with open('test_output.txt', 'r') as f:
+    #     assert f.read().strip('\n') == test_message
+
+
+    # os.remove('test_output.txt')
+def test_file_log():
+    test_message = 'test_message'
+    TestMagicLogger.info()
+
