c8091459d0585e9f53a92df6db83c72f8c0012dc
Nicolas <nicolasvo95@gmail.com>
2018-05-28 23:21:03 +0200

    update test

--- tests/test_magic_logger.py
+++ tests/test_magic_logger.py
@@ -6,20 +6,21 @@
 from magic_logger import MagicLogger
 import os
 
-TestMagicLogger = MagicLogger(logger_name='test_name', file_name='test_output.txt', host=None)
-
 def test_file_created():
+    TestMagicLogger = MagicLogger(logger_name='test_name', file_name='test_output.txt', host=None)
     assert isinstance(TestMagicLogger, MagicLogger)
     test_message = 'test_message'
     TestMagicLogger.info(test_message)
     assert os.path.exists('test_output.txt') == True
+    os.remove('test_output.txt')
 
-    # with open('test_output.txt', 'r') as f:
-    #     assert f.read().strip('\n') == test_message
-
-
-    # os.remove('test_output.txt')
-def test_file_log():
+def test_log_info():
+    TestMagicLogger = MagicLogger(logger_name='test_name', file_name='test_output.txt', host=None)
     test_message = 'test_message'
-    TestMagicLogger.info()
+    TestMagicLogger.info(test_message)
 
+    with open('test_output.txt', 'r') as f:
+        # assert f.read().split(' - ')[3].rstrip() == test_message
+        assert f.read().split(' - ')[2] == 'INFO'
+
+    os.remove('test_output.txt')
