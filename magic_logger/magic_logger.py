5d7057de73d8c0134e54be550c5af45bdf471954
Nicolas Vo <nicolas.vo@MBP-C02WC1F4HV2Q.local>
2018-06-08 13:05:34 +0200

    fix msg repetition

--- magic_logger/magic_logger.py
+++ magic_logger/magic_logger.py
@@ -62,7 +62,7 @@ class MagicLogger(CustomLogger):
                 self.log(level=ERROR, msg="Connection to logstash unsuccessful. ({0}:{1})".format(host, port))
 
     def decorate(self, msg="Example message", logstash=False, level=DEBUG):
-
+        msg_decorate = msg
         def _(f):
             def wrapper(*args, **kwargs):
                 before = datetime.datetime.now()
@@ -98,7 +98,7 @@ class MagicLogger(CustomLogger):
 
                 msg = f'{extra_decorate["function_class"] + " > " if extra_decorate.get("function_class") else ""}' \
                       f'{extra_decorate["function_name"] + " > "  if extra_decorate.get("function_name") else ""}' \
-                      f'{msg}'
+                      f'{msg_decorate}'
 
                 self.log(level=level, msg=msg.format(**kwargs),
                          extra_decorate=extra_decorate, logstash=logstash)
