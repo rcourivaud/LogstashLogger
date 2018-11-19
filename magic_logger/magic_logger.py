5b37e6ded83dc3523d18cef8a80ebe2c6c038224
Raphaël Courivaud <raph@getkudoz.com>
2018-11-19 17:32:20 +0100

    udpate magic logger decorate function

--- magic_logger/magic_logger.py
+++ magic_logger/magic_logger.py
@@ -1,7 +1,6 @@
 """Main module."""
 import sys
-from logging import Logger, DEBUG, INFO, CRITICAL, ERROR, WARNING, raiseExceptions, FileHandler, StreamHandler, \
-    Formatter, _checkLevel, addLevelName
+from logging import Logger, DEBUG, ERROR, FileHandler, StreamHandler, Formatter, _checkLevel, addLevelName
 
 from datetime import datetime
 from logstash import TCPLogstashHandler
@@ -14,13 +13,13 @@ import inspect
 _srcfile = os.path.normcase(addLevelName.__code__.co_filename)
 
 
-class LogstashLogger(Logger):
+class MagicLogger(Logger):
     def __init__(self, logger_name,
                  file_name=None,
                  host="logstash",
                  port=5000,
                  extra=None,
-                 blacklist=['self'],
+                 blacklist=None,
                  **kwargs):
         """
         :param logger_name:
@@ -32,6 +31,9 @@ class LogstashLogger(Logger):
         """
 
         super().__init__(name=logger_name)
+
+        if blacklist is None:
+            blacklist = ['self']
 
         self.extra = extra
 
@@ -100,7 +102,7 @@ class LogstashLogger(Logger):
                       f'{extra_decorate["function_name"] if extra_decorate.get("function_name") else ""}'
 
                 self.log(level=level, msg=msg.format(**kwargs),
-                         extra_decorate=extra_decorate, logstash=logstash)
+                         extra_=extra_decorate, logstash=logstash)
 
                 return res
             return wrapper
@@ -108,3 +110,46 @@ class LogstashLogger(Logger):
 
     def update_extra(self, **kwargs):
         self.extra = kwargs if self.extra is None else {**self.extra, **kwargs}
+
+    def log(self, level, msg, extra_=None, *args, **kwargs):
+        """
+        Log 'msg % args' with the integer severity 'level'.
+                self.log(level=level, msg=msg.format(**kwargs),
+                         extra_decorate=extra_decorate, logstash=logstash)
+         To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+         logger.log(level, "We have a %s", "mysterious problem", exc_info=1)
+        """
+        if self.isEnabledFor(_checkLevel(level)):
+            self._log(_checkLevel(level), msg, args, **kwargs)
+
+
+    def _log(self, level, msg, args, exc_info=None, extra=None, extra_=None, stack_info=False):
+        """
+        Low-level logging routine which creates a LogRecord and then calls
+        all the handlers of this logger to handle the record.
+        """
+        level = _checkLevel(level.upper()) if isinstance(level, str) else level
+        extra = {**(extra if extra else {}), **(extra_ if extra_ else {}), **(self.extra if self.extra else {})}
+        sinfo = None
+
+
+        if _srcfile:
+            # IronPython doesn't track Python frames, so findCaller raises an
+            # exception on some versions of IronPython. We trap it here so that
+            # IronPython can use logging.
+            try:
+                fn, lno, func, sinfo = self.findCaller(stack_info)
+            except ValueError:  # pragma: no cover
+                fn, lno, func = "(unknown file)", 0, "(unknown function)"
+        else:  # pragma: no cover
+            fn, lno, func = "(unknown file)", 0, "(unknown function)"
+        if exc_info:
+            if isinstance(exc_info, BaseException):
+                exc_info = (type(exc_info), exc_info, exc_info.__traceback__)
+            elif not isinstance(exc_info, tuple):
+                exc_info = sys.exc_info()
+        record = self.makeRecord(self.name, level, fn, lno, msg, args,
+                                 exc_info, func, extra, sinfo)
+        self.handle(record)
+
